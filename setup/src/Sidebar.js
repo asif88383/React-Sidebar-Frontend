54fdc06931f726f3b8c92f1451240b5222fee012
Muhammad Asif <asif88383@gmail.com>
2022-04-08 11:41:38 +0500

    first commit

--- setup/src/Sidebar.js
+++ setup/src/Sidebar.js
@@ -25,6 +25,17 @@ const Sidebar = () => {
           )
         })}
       </ul>
+
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const {id, url, icon} = link
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          )
+        })}
+      </ul>
     </aside>
   )
 }
