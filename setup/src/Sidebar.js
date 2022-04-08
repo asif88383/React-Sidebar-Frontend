d1d772eef9b5fa05a4d6356c059810b00b11f9c9
Muhammad Asif <asif88383@gmail.com>
2022-04-08 12:11:41 +0500

    first commit

--- setup/src/Sidebar.js
+++ setup/src/Sidebar.js
@@ -4,11 +4,12 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
+  const {isSidebarOpen, closeSidebar} = useGlobalContext()
   return (
-    <aside className="sidebar show-sidebar">
+    <aside className={`${isSidebarOpen? "sidebar show-sidebar": "sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className='logo' />
-        <button className='close-btn'>
+        <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
